e17da3903d999d1f6542271eb92663dc3e370c5a
Anwar Ramadha <anwarramadha22@gmail.com>
2024-08-16 15:42:34 +0700

    ci: :green_heart: fixing ci build errors

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
-import type { App } from 'vue'
 import { sicocoV3 } from '@sutekitechid/flowbite-vue'
 import '@sutekitechid/flowbite-vue/dist/style.css'
 
-export default (app: App) => {
+export default (app) => {
   app.use(sicocoV3)
 }
