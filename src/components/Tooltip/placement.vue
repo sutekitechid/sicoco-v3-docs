9f6ed061835dfca59947a8292500906e8f141a6d
Wirasya prima saputra <rasyap.s@MacBook-Air-Wirasya.local>
2024-09-23 14:27:25 +0700

    menambahkan tooltip

--- src/components/Tooltip/placement.vue
+++ src/components/Tooltip/placement.vue
@@ -1,72 +1,72 @@
 <template>
     <div style="display: flex; flex-wrap: wrap;">
-      <s-tooltip position="top" style="margin: 5px;" placement="top">
-        <template #trigger>
-          <s-button>Hover me! (Top)</s-button>
-        </template>
-        <template #content>Tooltip content (Top)</template>
-      </s-tooltip>
-      <s-tooltip position="top-start" style="margin: 5px;" placement="top-start">
+      <s-tooltip position="top" style="margin: 5px;" placement="top" trigger="mouseenter">
+      <template #trigger>
+        <s-button>Hover me! (Top)</s-button>
+      </template>
+      <template #content>Tooltip content (Top)</template>
+    </s-tooltip>
+      <s-tooltip position="top-start" style="margin: 5px;" placement="top-start" trigger="mouseenter">
         <template #trigger>
           <s-button>Hover me! (Top-Start)</s-button>
         </template>
         <template #content>Tooltip content (Top-Start)</template>
       </s-tooltip>
-      <s-tooltip position="top-end" style="margin: 5px;" placement="top-end">
+      <s-tooltip position="top-end" style="margin: 5px;" placement="top-end" trigger="mouseenter">
         <template #trigger>
           <s-button>Hover me! (Top-End)</s-button>
         </template>
         <template #content>Tooltip content (Top-End)</template>
       </s-tooltip>
-      <s-tooltip position="bottom" style="margin: 5px;" placement="bottom">
+      <s-tooltip position="bottom" style="margin: 5px;" placement="bottom" trigger="mouseenter">
         <template #trigger>
           <s-button>Hover me! (Bottom)</s-button>
         </template>
         <template #content>Tooltip content (Bottom)</template>
       </s-tooltip>
-      <s-tooltip position="bottom-start" style="margin: 5px;" placement="bottom-start">
+      <s-tooltip position="bottom-start" style="margin: 5px;" placement="bottom-start" trigger="mouseenter">
         <template #trigger>
           <s-button>Hover me! (Bottom-Start)</s-button>
         </template>
         <template #content>Tooltip content (Bottom-Start)</template>
       </s-tooltip>
-      <s-tooltip position="bottom-end" style="margin: 5px;" placement="bottom-end">
+      <s-tooltip position="bottom-end" style="margin: 5px;" placement="bottom-end" trigger="mouseenter">
         <template #trigger>
           <s-button>Hover me! (Bottom-End)</s-button>
         </template>
         <template #content>Tooltip content (Bottom-End)</template>
       </s-tooltip>
-      <s-tooltip position="left" style="margin: 5px;" placement="left">
+      <s-tooltip position="left" style="margin: 5px;" placement="left" trigger="mouseenter">
         <template #trigger>
           <s-button>Hover me! (Left)</s-button>
         </template>
         <template #content>Tooltip content (Left)</template>
       </s-tooltip>
-      <s-tooltip position="left-start" style="margin: 5px;" placement="left-start">
+      <s-tooltip position="left-start" style="margin: 5px;" placement="left-start" trigger="mouseenter">
         <template #trigger>
           <s-button>Hover me! (Left-Start)</s-button>
         </template>
         <template #content>Tooltip content (Left-Start)</template>
       </s-tooltip>
-      <s-tooltip position="left-end" style="margin: 5px;" placement="left-end">
+      <s-tooltip position="left-end" style="margin: 5px;" placement="left-end" trigger="mouseenter">
         <template #trigger>
           <s-button>Hover me! (Left-End)</s-button>
         </template>
         <template #content>Tooltip content (Left-End)</template>
       </s-tooltip>
-      <s-tooltip position="right" style="margin: 5px;" placement="right">
+      <s-tooltip position="right" style="margin: 5px;" placement="right" trigger="mouseenter">
         <template #trigger>
           <s-button>Hover me! (Right)</s-button>
         </template>
         <template #content>Tooltip content (Right)</template>
       </s-tooltip>
-      <s-tooltip position="right-start" style="margin: 5px;" placement="right-start">
+      <s-tooltip position="right-start" style="margin: 5px;" placement="right-start" trigger="mouseenter">
         <template #trigger>
           <s-button>Hover me! (Right-Start)</s-button>
         </template>
         <template #content>Tooltip content (Right-Start)</template>
       </s-tooltip>
-      <s-tooltip position="right-end" style="margin: 5px;" placement="right-end">
+      <s-tooltip position="right-end" style="margin: 5px;" placement="right-end" trigger="mouseenter">
         <template #trigger>
           <s-button>Hover me! (Right-End)</s-button>
         </template>
---
+++ src/components/Tooltip/triger.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <s-tooltip position="top" style="margin: 5px; display: inline-block;" placement="top" trigger="click">
+      <template #trigger>
+        <s-button>Click me!</s-button>
+      </template>
+      <template #content>Tooltip content</template>
+    </s-tooltip>
+    <s-tooltip position="right" style="margin: 5px; display: inline-block;" placement="top" trigger="mouseenter">
+      <template #trigger>
+        <s-button>Hover me!</s-button>
+      </template>
+      <template #content>Tooltip content</template>
+    </s-tooltip>
+  </div>
+</template>
