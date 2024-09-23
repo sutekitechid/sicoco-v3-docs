20c713e93d6c3bc00f5a460ae9a25262e1c773f9
Wirasya prima saputra <rasyap.s@MacBook-Air-Wirasya.local>
2024-09-23 08:42:28 +0700

    menambahkan tooltip

---
+++ src/components/Tooltip/apis/apiSlots.vue
@@ -0,0 +1,19 @@
+<template>
+  <ApiSlotTable :slots="data" />
+</template>
+
+<script setup>
+import ApiSlotTable from '@/components/api-table/apiSlot.vue'
+const data = [
+  {
+    "name": "trigger",
+    "description": "content that will be hovered",
+    "props": ""
+  },
+  {
+    "name": "content",
+    "description": "content that appears after trigger is hovered",
+    "props": ""
+  }
+]
+</script>
