978011465b9427424ff2495cf146b33015cba6ed
Iyas Bagus <150093945+iyasbagus@users.noreply.github.com>
2024-09-24 08:41:12 +0700

    Add badge component (#8)
    * Add badge component
    
    * menambahkan dark-danger dan grey
    
    ---------
    
    Co-authored-by: iyas bagus <bagusiyas15@gmail.com>

---
+++ src/components/Badge/apis/apiProperties.vue
@@ -0,0 +1,39 @@
+<template>
+    <ApiPropertyTable :properties="data" />
+</template>
+
+<script setup>
+import ApiPropertyTable from '@/components/api-table/apiProperty.vue'
+const data = [
+    {
+        name: 'type',
+        type: 'string',
+        default: 'primary',
+        values: [
+            'primary',
+            'success',
+            'warning',
+            'danger',
+            'dark-danger',
+            'grey',
+            'secondary',
+            'purple'
+        ],
+        description: 'Changing the badge color.',
+    },
+    {
+        name: 'rounded',
+        type: 'boolean',
+        default: 'false',
+        values: '',
+        description: 'Making a badge oval.',
+    },
+    {
+        name: 'closable',
+        type: 'boolean',
+        default: 'false',
+        values: '',
+        description: 'Add a close icon to allow the badge to be closed.',
+    },
+]
+</script>
---
+++ src/components/Badge/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex gap-2">
+        <s-badge type="primary">Badge</s-badge>
+        <s-badge type="success">Badge</s-badge>
+        <s-badge type="warning">Badge</s-badge>
+        <s-badge type="danger">Badge</s-badge>
+        <s-badge type="dark-danger">Badge</s-badge>
+        <s-badge type="grey">Badge</s-badge>
+        <s-badge type="secondary">Badge</s-badge>
+        <s-badge type="purple">Badge</s-badge>
+    </div>
+</template>
