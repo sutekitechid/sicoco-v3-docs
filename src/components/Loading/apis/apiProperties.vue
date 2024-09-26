543932a264b59ee99a2a8887f091a0be00b5ac91
iyas bagus <bagusiyas15@gmail.com>
2024-09-26 12:41:21 +0700

    menambahkan loading

---
+++ src/components/Loading/apis/apiProperties.vue
@@ -0,0 +1,21 @@
+<template>
+  <ApiPropertyTable :properties="data" />
+</template>
+
+<script setup>
+import ApiPropertyTable from '@/components/api-table/apiProperty.vue'
+const data = [
+  {
+    name: 'onCancel',
+    type: 'boolean',
+    default: '',
+    description: 'To display a different background for each row',
+    },
+  {
+    name: 'active',
+    type: 'boolean',
+    default: '',
+    description: 'To make a table scrollable if the content is too long',
+  },
+]
+</script>
