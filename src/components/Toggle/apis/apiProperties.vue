43a439071d415d085910a195837163c8129866c9
Wirasya prima saputra <rasyap.s@MacBook-Air-Wirasya.local>
2024-10-02 10:49:13 +0700

    menambahkan component toggle

---
+++ src/components/Toggle/apis/apiProperties.vue
@@ -0,0 +1,31 @@
+<template>
+  <ApiPropertyTable :properties="data" />
+</template>
+
+<script setup>
+import ApiPropertyTable from '@/components/api-table/apiProperty.vue'
+
+const data = [
+  {
+  name: 'true-value',
+  type: 'boolean/string',
+  description: '',
+  values: '',
+  default: false,
+},
+{
+  name: 'false-value',
+  type: 'boolean/string',
+  description: '',
+  values: '',
+  default: false,
+},
+{
+  name: 'disabled',
+  type: 'boolean',
+  description: 'disabled toggle',
+  values: '',
+  default: false,
+},
+]
+</script>
---
+++ src/components/Toggle/apis/apiSlots.vue
@@ -0,0 +1,14 @@
+<template>
+  <ApiSlotTable :slots="data" />
+</template>
+
+<script setup>
+import ApiSlotTable from '@/components/api-table/apiSlot.vue'
+const data = [
+  {
+    "name": "default",
+    "description": "displays content next to the toggle",
+    "props": ""
+  },
+]
+</script>
