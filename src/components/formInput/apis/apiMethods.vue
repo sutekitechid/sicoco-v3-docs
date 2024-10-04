e6e2a8e6ab8b7cdfce36ace3d889bfe8edafbefc
iyas bagus <bagusiyas15@gmail.com>
2024-10-04 09:54:56 +0700

    menambahkan form input

---
+++ src/components/formInput/apis/apiMethods.vue
@@ -0,0 +1,33 @@
+<template>
+  <ApiMethodTable :methods="data" />
+</template>
+
+<script setup>
+import ApiMethodTable from '@/components/api-table/apiMethod.vue'
+const data = [
+  {
+    name: 'registerValidateFunc',
+    description: 'Register validate function from each child component such as InputText, InputSelect, dropdown, etc',
+    parameters: 'function',
+    return: '',
+  },
+  {
+    name: 'removeValidateFunc',
+    description: 'Remove validaton function that was added',
+    parameters: 'id',
+    return: '',
+  },
+  {
+    name: 'validate',
+    description: 'Validating the form before submitting',
+    parameters: '',
+    return: '',
+    },
+  {
+    name: 'reset',
+    description: 'Reset validation',
+    parameters: '',
+    return: '',
+  },
+]
+</script>
