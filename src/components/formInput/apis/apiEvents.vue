42fb8c97c853f0c289220b77f400771c9429029b
Iyas Bagus <150093945+iyasbagus@users.noreply.github.com>
2024-10-07 08:49:51 +0700

    menambahkan form input (#17)
    * menambahkan form input
    
    * menambahkan form input
    
    ---------
    
    Co-authored-by: iyas bagus <bagusiyas15@gmail.com>

---
+++ src/components/formInput/apis/apiEvents.vue
@@ -0,0 +1,14 @@
+<template>
+  <ApiEventTable :events="data" />
+</template>
+
+<script setup>
+import ApiEventTable from '@/components/api-table/apiEvent.vue'
+const data = [
+  {
+    name: 'submit',
+    description: 'Submitting the form',
+    parameters: '',
+  },
+]
+</script>
---
+++ src/components/formInput/apis/apiProperties.vue
@@ -0,0 +1,23 @@
+<template>
+    <ApiPropertyTable :properties="data" />
+</template>
+
+<script setup>
+import ApiPropertyTable from '@/components/api-table/apiProperty.vue'
+const data = [
+    {
+        name: 'label',
+        type: 'string',
+        default: 'undefined',
+        values: '',
+        description: 'Total pages available',
+    },
+    {
+        name: 'autoComplete',
+        type: 'string',
+        default: 'off',
+        values: '',
+        description: 'Data displayed on each page',
+    },
+]
+</script>
