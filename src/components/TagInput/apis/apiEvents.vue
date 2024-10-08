cf4f46f9c5fc4dc10c2cc5e9a5180de2ed72f7f3
iyas bagus <bagusiyas15@gmail.com>
2024-10-08 10:53:10 +0700

    menambahkan component taginput

---
+++ src/components/TagInput/apis/apiEvents.vue
@@ -0,0 +1,20 @@
+<template>
+    <apiEventTable :events="data"/>
+</template>
+
+<script setup>
+import ApiEventTable from '@/components/api-table/apiEvent.vue'
+
+const data = [
+    {
+        name: 'typing',
+        description: 'Ditrigger ketika user memasukkan input',
+        parameters: 'event: $event'
+    },
+    {
+        name: 'keypress',
+        description: 'Ditrigger ketika user menekan tombol',
+        parameters: 'event: $event'
+    },
+]
+</script>
---
+++ src/components/TagInput/customTag.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="mt-5">
+    <STaginput
+      v-model="input"
+      autocomplete
+      placeholder="Input Tag"
+      @keypress.enter.space="addTag">
+    <template #tag>
+        <p class="text-danger-60">Hello</p>
+    </template>
+
+    </STaginput>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const input = ref(['Apple']);
+
+const addTag = (event) => {
+  const newTag = event.target.value.trim();
+  if (newTag && !input.value.includes(newTag)) {
+    input.value.push(newTag);
+    event.target.value = '';
+  }
+};
+</script>
