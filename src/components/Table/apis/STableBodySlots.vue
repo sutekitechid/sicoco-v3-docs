4301299db020a18ddf5ee3d641c2ec267197cd45
wirasya01 <147147327+wirasya01@users.noreply.github.com>
2024-10-09 14:23:59 +0700

    menambahkan component tabel (#21)
    * pagination
    
    * menambahkan component table

---
+++ src/components/Table/apis/STableBodySlots.vue
@@ -0,0 +1,14 @@
+<template>
+  <ApiSlotTable :slots="data" />
+</template>
+
+<script setup>
+import ApiSlotTable from '@/components/api-table/apiSlot.vue'
+const data = [
+  {
+    "name": "default",
+    "description": "To display a different background on each row",
+    "props": ""
+  },
+]
+</script>
---
+++ src/components/Table/apis/apiProperties.vue
@@ -0,0 +1,52 @@
+<template>
+  <ApiPropertyTable :properties="data" />
+</template>
+
+<script setup>
+import ApiPropertyTable from '@/components/api-table/apiProperty.vue'
+
+const data = [
+  {
+    name: 'stripped',
+    type: 'boolean',
+    description: 'To display a different background on each row',
+    values: '',
+    default: false,
+  },
+  {
+    name: 'scrollable',
+    type: 'boolean',
+    description: 'Makes the table scrollable if the content is too long',
+    values: '',
+    default: false,
+  },
+  {
+    name: 'fullwidth',
+    type: 'boolean',
+    description: 'Makes the table size 100%',
+    values: '',
+    default: false,
+  },
+  {
+    name: 'stripped',
+    type: 'boolean',
+    description: 'Displays a different background on each column',
+    values: '',
+    default: false,
+  },
+  {
+    name: 'hoverable',
+    type: 'boolean',
+    description: 'Displays an effect when the cursor is pointed at the table',
+    values: '',
+    default: false,
+  },
+  {
+    name: 'topHorizontalScroll',
+    type: 'boolean',
+    description: 'Displays a scrollbar at the top of the header',
+    values: '',
+    default: false,
+  },
+]
+</script>
