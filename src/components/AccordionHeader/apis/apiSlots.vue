cd8e1421f1843b7d2d88b027d09bb27b0c962351
Iyas Bagus <150093945+iyasbagus@users.noreply.github.com>
2024-10-09 15:19:17 +0700

    menambahkan component accordion (#22)
    * menambahkan component accordion
    
    * menambahkan component accordion
    
    ---------
    
    Co-authored-by: iyas bagus <bagusiyas15@gmail.com>

---
+++ src/components/AccordionHeader/apis/apiSlots.vue
@@ -0,0 +1,14 @@
+<template>
+  <ApiSlotTable :slots="data" />
+</template>
+
+<script setup>
+import ApiSlotTable from '@/components/api-table/apiSlot.vue'
+const data = [
+  {
+    name: 'default',
+    description: 'Functions as a button or clickable area for users to open or close the content.',
+    props: '',
+    },
+]
+</script>
