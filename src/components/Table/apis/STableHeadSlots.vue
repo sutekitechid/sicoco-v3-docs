ebf2eb2b42b4d641dc87ebd2bb8f08bc81f90a8c
wirasya01 <rasyaprimasaputra@gmail.com>
2024-10-09 14:14:24 +0700

    menambahkan component table

---
+++ src/components/Table/apis/STableHeadSlots.vue
@@ -0,0 +1,14 @@
+<template>
+  <ApiSlotTable :slots="data" />
+</template>
+
+<script setup>
+import ApiSlotTable from '@/components/api-table/apiSlot.vue'
+const data = [
+  {
+    "name": "default",
+    "description": "Used to wrap content in the table header",
+    "props": ""
+  },
+]
+</script>
