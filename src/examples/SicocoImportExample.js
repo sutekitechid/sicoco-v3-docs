17553a43c2056b527b63e06b0262024a045f78dd
Anwar Ramadha <anwarramadha22@gmail.com>
2025-05-15 16:52:09 +0700

    feat(installation-and-setup): :sparkles: added installation step

--- src/examples/SicocoImportExample.js
+++ src/examples/SicocoImportExample.js
@@ -1,30 +1,9 @@
-import { defineNuxtPlugin } from 'nuxt/app'
-import { sicocoV3 } from '@sutekitechid/flowbite-vue'
-import inputId from '../locales/input/id'
-import inputEn from '../locales/input/en'
-import commonsId from '../locales/commons/id'
-import commonsEn from '../locales/commons/en'
-import errorHandlingId from '../locales/error-handling/id'
-import errorHandlingEn from '../locales/error-handling/en'
-
-import '@sutekitechid/flowbite-vue/icomoon/style.css'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(sicocoV3, {
-    i18n: {
-      enabled: false,
-      locales: {
-        id: {
-          ...inputId,
-          ...commonsId,
-          ...errorHandlingId
-        },
-        en: {
-          ...inputEn,
-          ...commonsEn,
-          ...errorHandlingEn
-        }
-      }
-    }
-  })
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	...
+	css: [
+		'@sutekitechid/sicoco-v3-next/dist/assets/sicoco-v3-next.css',
+		'@sutekitechid/sicoco-v3-next/dist/assets/icomoon/style.css',
+	],
+  ...
 })
