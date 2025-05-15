9c6e60f954b81cb6f6f5949507fe08980d98fa95
maulanairfanf <maulanairfanf@gmail.com>
2025-05-15 16:26:29 +0700

    fix(badge): :bug: CU-86cyypqcx fix badge documentation

--- src/components/Badge/rounded.vue
+++ src/components/Badge/rounded.vue
@@ -1,12 +1,23 @@
 <template>
-    <div class="flex gap-2">
-        <s-badge rounded type="primary">Badge</s-badge>
-        <s-badge rounded type="success">Badge</s-badge>
-        <s-badge rounded type="warning">Badge</s-badge>
-        <s-badge rounded type="danger">Badge</s-badge>
-        <s-badge rounded type="dark-danger">Badge</s-badge>
-        <s-badge rounded type="grey">Badge</s-badge>
-        <s-badge rounded type="secondary">Badge</s-badge>
-        <s-badge rounded type="purple">Badge</s-badge>
-    </div>
+	<div class="flex gap-2" id="component-sicoco">
+		<s-badge rounded variant="primary">Badge</s-badge>
+		<s-badge rounded variant="success">Badge</s-badge>
+		<s-badge rounded variant="warning">Badge</s-badge>
+		<s-badge rounded variant="danger">Badge</s-badge>
+		<s-badge rounded variant="dark-danger">Badge</s-badge>
+		<s-badge rounded variant="grey">Badge</s-badge>
+		<s-badge rounded variant="secondary">Badge</s-badge>
+		<s-badge rounded variant="purple">Badge</s-badge>
+	</div>
 </template>
+
+<script lang="ts">
+import { SButton, SBadge } from '@sutekitechid/sicoco-v3-next'
+
+export default {
+	components: {
+		SButton,
+		SBadge,
+	},
+}
+</script>
