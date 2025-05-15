4f73770c54f1490756517c3cb923cbf3942a4965
Anwar Ramadha <anwarramadha22@gmail.com>
2025-05-15 11:10:16 +0700

    bump sicoco to 3.5.3

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,2 +1,3 @@
 import '@sutekitechid/sicoco-v3-next/dist/assets/sicoco-v3-next.css'
 import '@sutekitechid/sicoco-v3-next/dist/assets/icomoon/style.css'
+import '../styles/global.css'
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['selector', '[data-theme="dark"]'],
-  theme: {
-    extend: {},
-  },
-  content: [
-    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-    './public/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-    './node_modules/@sutekitechid/sicoco-v3-next/dist/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-  ],
-  presets: [require('./node_modules/@sutekitechid/sicoco-v3-next/dist/config/configPreset.js')]
+	darkMode: ['selector', '[data-theme="dark"]'],
+	theme: {
+		extend: {},
+	},
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./public/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/@sutekitechid/sicoco-v3-next/dist/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+	],
+	presets: [
+		require('./node_modules/@sutekitechid/sicoco-v3-next/dist/config/configPreset.js'),
+	],
 }
