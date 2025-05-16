0b59917606e7b878eb7d454473cd12cd4d9fa89d
maulanairfanf <maulanairfanf@gmail.com>
2025-05-16 08:55:14 +0700

    fix(alert): :memo: CU-86cyypr4h add alert documentation

---
+++ src/components/Alert/variant.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="flex flex-col gap-4" id="component-sicoco">
+		<SAlert variant="info">Info Alert</SAlert>
+		<SAlert variant="success">Success Alert</SAlert>
+		<SAlert variant="warning">Warning Alert</SAlert>
+		<SAlert variant="danger">Danger Alert</SAlert>
+		<SAlert variant="primary">Primary Alert</SAlert>
+	</div>
+</template>
+
+<script setup>
+import { SAlert } from '@sutekitechid/sicoco-v3-next'
+</script>
