7f8398ddfafb30f5cafcc8a4ebe18fc7fd27a9c9
maulanairfanf <maulanairfanf@gmail.com>
2025-05-16 09:26:01 +0700

    feat(switch): :memo: CU-86cyypubd add documentation switch

---
+++ src/components/Switch/apis/apiProperties.vue
@@ -0,0 +1,53 @@
+<template>
+	<ApiPropertyTable :properties="data" />
+</template>
+
+<script setup>
+import ApiPropertyTable from '@/components/api-table/apiProperty.vue'
+
+const data = [
+	{
+		name: 'class',
+		type: 'string',
+		default: "''",
+		values: '',
+		description: 'Additional CSS classes for the root element of the switch.',
+	},
+	{
+		name: 'modelValue',
+		type: 'boolean | string',
+		default: 'false',
+		values: '',
+		description:
+			'The current value of the switch for two-way binding (v-model).',
+	},
+	{
+		name: 'trueValue',
+		type: 'boolean | string',
+		default: 'true',
+		values: '',
+		description: 'The value considered as "checked".',
+	},
+	{
+		name: 'falseValue',
+		type: 'boolean | string',
+		default: 'false',
+		values: '',
+		description: 'The value considered as "unchecked".',
+	},
+	{
+		name: 'disabled',
+		type: 'boolean',
+		default: 'false',
+		values: '',
+		description: 'Determines if the switch is disabled.',
+	},
+	{
+		name: 'variant',
+		type: 'string',
+		default: "'primary'",
+		values: ['primary', 'success', 'warning', 'danger', 'info', 'secondary'],
+		description: 'The variant of the switch, which determines the color.',
+	},
+]
+</script>
