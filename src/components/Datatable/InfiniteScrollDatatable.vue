c664ecb0a057eb7e51e1725654e81401bc13e984
Anwar Ramadha <anwarramadha22@gmail.com>
2025-05-19 16:54:57 +0700

    Feat/Cu-86cyyprxd--DOCS]-Sicoco-v3-next---Datatable (#48)
    * feat(table): :sparkles: added datatable docs
    
    * feat: :sparkles: added conventional commit type

---
+++ src/components/Datatable/InfiniteScrollDatatable.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="component-sicoco">
+		<Datatable
+			id="default-datatable"
+			:data="coffe.hot"
+			:paginated="paginated"
+			:selectable="selectable"
+			infinite-scroll
+		></Datatable>
+	</div>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import Datatable from './Datatable.vue'
+import coffe from './coffee.json'
+
+const paginated = ref(false)
+const selectable = ref(false)
+</script>
---
+++ src/components/Datatable/apis/apiTableEvents.vue
@@ -0,0 +1,32 @@
+<template>
+	<ApiEventTable :events="data" />
+</template>
+
+<script setup>
+import ApiEventTable from '@/components/api-table/apiEvent.vue'
+const data = [
+	{
+		name: 'update:modelValue',
+		description:
+			'Emitted when the selected rows change (use with v-model="selectedRows").',
+		parameters: 'value: any',
+	},
+	{
+		name: 'change-page',
+		description:
+			'Emitted when the page changes (use with @change-page="onPageChange").',
+		parameters: 'value: number',
+	},
+	{
+		name: 'change-per-page',
+		description:
+			'Emitted when the number of items per page changes (use with @change-per-page="onPerPageChange").',
+		parameters: 'value: number',
+	},
+	{
+		name: 'sort',
+		description: 'Emitted when the table is sorted (use with @sort="onSort").',
+		parameters: 'id: string, desc: boolean',
+	},
+]
+</script>
