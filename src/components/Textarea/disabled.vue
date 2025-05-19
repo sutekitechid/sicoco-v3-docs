74ea3173b99957085633dec4d3c5bdf2bb539ce9
Resha Puspita <reshapuspita734@gmail.com>
2025-05-19 16:40:24 +0700

    feat(textarea): modify docs (#45)

--- src/components/Textarea/disabled.vue
+++ src/components/Textarea/disabled.vue
@@ -1,13 +1,17 @@
 <template>
-    <s-textarea 
-        v-model="input"
-        placeholder="John Doe"
-        disabled
-    />
+	<s-textarea
+		v-model="input"
+		class="mt-4"
+		placeholder="Enter your text here"
+		maxlength="30"
+		minlength="5"
+		disabled
+	/>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { STextarea } from '@sutekitechid/sicoco-v3-next'
 
 const input = ref('')
 </script>
