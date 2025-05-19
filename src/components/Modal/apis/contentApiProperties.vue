d42638e184be4cf87f5321ff7591825666e0f49e
resha <reshapuspita734@gmail.com>
2025-05-19 16:46:00 +0700

    feat(dialog): modify Dialog api reference

---
+++ src/components/Modal/apis/contentApiProperties.vue
@@ -0,0 +1,24 @@
+<template>
+	<ApiPropertyTable :properties="data" />
+</template>
+
+<script setup>
+import ApiPropertyTable from '@/components/api-table/apiProperty.vue'
+const data = [
+	{
+		name: 'class',
+		type: 'string',
+		default: "''",
+		values: '',
+		description:
+			'Custom CSS classes to style and shape the Dialog Content component',
+	},
+	{
+		name: 'z-index',
+		type: 'string',
+		default: '50',
+		values: '',
+		description: 'Z-index of the component',
+	},
+]
+</script>
