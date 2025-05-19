bac63e3c98de1d711877b42eaca1f32cd583afbd
Maul <50138748+maulanairfanf@users.noreply.github.com>
2025-05-19 09:01:32 +0700

    Feat/cu 86cyypubd switch documentation (#39)
    * feat(switch): :memo: CU-86cyypubd add documentation switch
    
    * add new scope
    
    * fix(switch): :pencil2: fix variant  values

---
+++ src/components/Switch/apis/apiSlots.vue
@@ -0,0 +1,14 @@
+<template>
+	<ApiSlotTable :slots="data" />
+</template>
+
+<script setup>
+import ApiSlotTable from '@/components/api-table/apiSlot.vue'
+
+const data = [
+	{
+		name: 'default',
+		description: 'Slot for custom content inside the switch.',
+	},
+]
+</script>
