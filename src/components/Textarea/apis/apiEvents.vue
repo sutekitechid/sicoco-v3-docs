5cfdca323ef92b59901adfed04882f93b065c12c
resha <reshapuspita734@gmail.com>
2025-05-19 14:38:41 +0700

    feat(textarea): modify docs

---
+++ src/components/Textarea/apis/apiEvents.vue
@@ -0,0 +1,24 @@
+<template>
+	<ApiEventTable :events="data" />
+</template>
+
+<script setup>
+import ApiEventTable from '@/components/api-table/apiEvent.vue'
+const data = [
+	{
+		name: 'focus',
+		description: 'The textarea is focused',
+		parameters: 'event: $event',
+	},
+	{
+		name: 'blur',
+		description: 'The textarea is blurred',
+		parameters: 'event: $event',
+	},
+	{
+		name: 'input',
+		description: 'The textarea is changed',
+		parameters: 'value: string | number',
+	},
+]
+</script>
