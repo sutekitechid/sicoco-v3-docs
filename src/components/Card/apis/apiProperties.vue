53a4f4e750c200b6baf1049cf2acfe336b7b489e
resha <reshapuspita734@gmail.com>
2025-05-20 14:52:43 +0700

    feat(card): modify docs

---
+++ src/components/Card/apis/apiProperties.vue
@@ -0,0 +1,35 @@
+<template>
+	<ApiPropertyTable :properties="data" />
+</template>
+
+<script setup>
+import ApiPropertyTable from '@/components/api-table/apiProperty.vue'
+
+const data = [
+	{
+		name: 'class',
+		type: 'string',
+		description: 'Additional custom CSS classes.',
+		values: [],
+		default: "''",
+	},
+	{
+		name: 'shadow',
+		type: 'boolean',
+		description: 'Add shadow to the card',
+		default: 'false',
+	},
+	{
+		name: 'border',
+		type: 'boolean',
+		description: 'Add border to the card',
+		default: 'false',
+	},
+	{
+		name: 'rounded',
+		type: 'boolean',
+		description: 'Border radius of the card.',
+		default: 'false',
+	},
+]
+</script>
