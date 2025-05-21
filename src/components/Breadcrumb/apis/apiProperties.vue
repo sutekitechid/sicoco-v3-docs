49bb011d0aa97d28b05c923821e35a2a488fc611
resha <reshapuspita734@gmail.com>
2025-05-21 11:03:37 +0700

    feat(breadcrumb): :sparkles: add breadcrumb docs

---
+++ src/components/Breadcrumb/apis/apiProperties.vue
@@ -0,0 +1,18 @@
+<template>
+	<ApiPropertyTable :properties="data" />
+</template>
+
+<script setup>
+import ApiPropertyTable from '@/components/api-table/apiProperty.vue'
+
+const data = [
+	{
+		name: 'class',
+		type: 'string',
+		default: "''",
+		values: '',
+		description:
+			'Additional CSS classes for the root element of the breadcrumb.',
+	},
+]
+</script>
---
+++ src/components/Breadcrumb/apis/breadcrumbItemApiProperties.vue
@@ -0,0 +1,40 @@
+<template>
+	<ApiPropertyTable :properties="data" />
+</template>
+
+<script setup>
+import ApiPropertyTable from '@/components/api-table/apiProperty.vue'
+
+const data = [
+	{
+		name: 'class',
+		type: 'string',
+		default: "''",
+		values: '',
+		description:
+			'Additional CSS classes for the root element of the breadcrumb.',
+	},
+	{
+		name: 'to',
+		type: 'string | unknown',
+		values: '',
+		description: 'The target route for the breadcrumb.',
+	},
+	{
+		name: 'disabled',
+		type: 'boolean',
+		description:
+			'If true, the breadcrumb item will be disabled and not clickable.',
+	},
+	{
+		name: 'as',
+		type: 'string',
+		description: 'The HTML element to use for the breadcrumb item.',
+	},
+	{
+		name: 'target',
+		type: 'string',
+		description: 'The target attribute for the link.',
+	},
+]
+</script>
