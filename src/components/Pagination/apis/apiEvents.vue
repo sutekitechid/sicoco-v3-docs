d7ee9a9032efb784ec737406eb458a2e228d3969
Dika Rizky Fawzi <dikarfawzi@gmail.com>
2025-05-21 09:31:47 +0700

    Feat/CU-86cyyptjt-pagination (#55)
    * feat(pagination): :sparkles: CU-86cyyptjt pagination docs
    
    * feat(pagination): :sparkles: CU-86cyyptjt items per page docs

--- src/components/Pagination/apis/apiEvents.vue
+++ src/components/Pagination/apis/apiEvents.vue
@@ -1,24 +1,36 @@
 <template>
-  <ApiEventTable :events="data" />
+	<ApiEventTable :events="data" />
 </template>
 
 <script setup>
 import ApiEventTable from '@/components/api-table/apiEvent.vue'
+const emit = defineEmits([
+	'update:perPage',
+	'update:page',
+	'change-page',
+	'change-per-page',
+])
+
 const data = [
-  {
-    name: 'v-model',
-    description: 'Handle page change',
-    parameters: '',
-  },
-  {
-    name: 'v-model:per-page',
-    description: 'Handle per page change',
-    parameters: '',
-  },
-  {
-    name: 'v-model:show-all',
-    description: 'Handle per page change but only show all',
-    parameters: '',
-  },
+	{
+		name: 'update:perPage',
+		description: 'Emitted when the per page value changes',
+		parameters: 'perPage: number',
+	},
+	{
+		name: 'update:page',
+		description: 'Emitted when the page value changes',
+		parameters: 'page: number',
+	},
+	{
+		name: 'change-page',
+		description: 'Emitted when the page value changes',
+		parameters: 'page: number',
+	},
+	{
+		name: 'change-per-page',
+		description: 'Emitted when the per page value changes',
+		parameters: 'perPage: number',
+	},
 ]
 </script>
--- src/components/Pagination/apis/apiProperties.vue
+++ src/components/Pagination/apis/apiProperties.vue
@@ -1,93 +1,75 @@
 <template>
-    <ApiPropertyTable :properties="data" />
+	<ApiPropertyTable :properties="data" />
 </template>
 
 <script setup>
 import ApiPropertyTable from '@/components/api-table/apiProperty.vue'
+
 const data = [
-    {
-        name: 'total-pages',
-        type: 'number',
-        description: 'Total available pages',
-        values: '',
-        default: 1,
-    },
-    {
-        name: 'per-page',
-        type: 'number',
-        description: 'Data displayed per page',
-        values: '',
-        default: 10,
-    },
-    {
-        name: 'show-all',
-        type: 'boolean',
-        description: 'Display all data',
-        values: '',
-        default: false,
-    },
-    {
-        name: 'total-items',
-        type: 'number',
-        description: 'Total data count',
-        values: '',
-        default: false,
-    },
-    {
-        name: 'per-page-options',
-        type: 'array',
-        description: 'Options for displaying data per page',
-        values: '',
-        default: [10, 20, 50, 100],
-    },
-    {
-        name: 'enable-all-per-page',
-        type: 'boolean',
-        description: '',
-        values: '',
-        default: false,
-    },
-    {
-        name: 'use-per-page',
-        type: 'boolean',
-        description: '',
-        values: '',
-        default: true,
-    },
-    {
-        name: 'layout',
-        type: 'string',
-        description: '',
-        values: '',
-        default: '',
-    },
-    {
-        name: 'show-icons',
-        type: 'boolean',
-        description: '',
-        values: '',
-        default: false,
-    },
-    {
-        name: 'slice-length',
-        type: 'number',
-        description: '',
-        values: '',
-        default: 0,
-    },
-    {
-        name: 'previous-label',
-        type: 'string',
-        description: '',
-        values: '',
-        default: '',
-    },
-    {
-        name: 'next-label',
-        type: 'string',
-        description: '',
-        values: '',
-        default: '',
-    },
+	{
+		name: 'total',
+		type: 'number | string',
+		description: 'Total data',
+		values: '',
+		default: 0,
+	},
+	{
+		name: 'perPage',
+		type: 'number | string',
+		description: 'Number of items per page',
+		values: '',
+		default: 20,
+	},
+	{
+		name: 'page',
+		type: 'number | string',
+		description: 'Current page number',
+		values: '',
+		default: 1,
+	},
+	{
+		name: 'defaultPage',
+		type: 'number | string',
+		description: 'Default page number',
+		values: '',
+		default: 1,
+	},
+	{
+		name: 'options',
+		type: 'number[]',
+		description: 'Array of options for items per page',
+		values: '',
+		default: undefined,
+	},
+	{
+		name: 'perPageLabelText',
+		type: 'string',
+		description: 'Label text for items per page dropdown',
+		values: '',
+		default: undefined,
+	},
+	{
+		name: 'perPageItemFormatter',
+		type: '(perPage) => string',
+		description:
+			'Function to format the items per page label text. Receives the number of items as an argument.',
+		values: '',
+		default: undefined,
+	},
+	{
+		name: 'showPerPageOptions',
+		type: 'boolean',
+		description:
+			'Whether to show the options for items per page dropdown or not.',
+		values: '',
+		default: true,
+	},
+	{
+		name: 'showPaginationInput',
+		type: 'boolean',
+		description: 'Whether to show the pagination input field or not.',
+		values: '',
+		default: true,
+	},
 ]
 </script>
