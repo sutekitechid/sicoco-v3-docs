3e12f838e43ea28757e5dae38a2c84e81d936e3b
Resha Puspita <reshapuspita734@gmail.com>
2025-05-21 11:45:00 +0700

    feat(breadcrumb): :sparkles: add breadcrumb docs (#60)

---
+++ src/components/Breadcrumb/disabled.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="flex flex-wrap gap-4 mt-4">
+		<SBreadcrumb>
+			<SBreadcrumbItem
+				v-for="item in labels"
+				:to="!item.disabled ? item.href : undefined"
+				:disabled="item.disabled"
+				>{{ item.name }}</SBreadcrumbItem
+			>
+		</SBreadcrumb>
+	</div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { SBreadcrumb, SBreadcrumbItem } from '@sutekitechid/sicoco-v3-next'
+
+const labels = ref([
+	{
+		name: 'Home',
+		href: '/sicoco-v3-docs',
+	},
+	{
+		name: 'Checkbox',
+		href: '',
+		disabled: true,
+	},
+])
+</script>
+
+<style>
+.sl-markdown-content li {
+	@apply !mt-0;
+}
+</style>
