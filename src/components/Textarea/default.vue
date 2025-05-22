13a990467c99b106952ec72ca1afc5251d1d8419
resha <reshapuspita734@gmail.com>
2025-05-22 10:18:52 +0700

    refactor(textarea): :lipstick: add not-content class

--- src/components/Textarea/default.vue
+++ src/components/Textarea/default.vue
@@ -1,13 +1,14 @@
 <template>
-	<s-textarea
-		v-model="input"
-		class="mt-4"
-		placeholder="Enter your text here"
-		maxlength="30"
-		minlength="5"
-	>
-		<template #minlength> Minimal 5 karakter</template></s-textarea
-	>
+	<div class="not-content">
+		<s-textarea
+			v-model="input"
+			placeholder="Enter your text here"
+			maxlength="30"
+			minlength="5"
+		>
+			<template #minlength> Minimal 5 karakter</template></s-textarea
+		>
+	</div>
 </template>
 
 <script setup>
